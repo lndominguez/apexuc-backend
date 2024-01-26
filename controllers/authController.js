57ab9f59e5587c993fe5df90a69de3fd13ec863f
Leandro <lndominguez12@gmail.com>
2024-01-26 06:16:19 -0800

    login with google

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,67 +1,114 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
+import axios from 'axios';
 import bcryptjs from 'bcryptjs';
 
 
 
 async function register(req, res) {
-
-    const { firstName, lastName, email, password } = req.body
-
-    try {
-
-        const userFound = await User.findOne({ email })
-
-        if (userFound) return res.status(401).json({message: 'This email has been taken'})
-
-        const hashedPassword = await bcryptjs.hash(password, 10);
-
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword
-        })
-
-        const userSaved = await newUser.save()
-
-
-        return res.json({
-            accessToken: token, user: {
-                id: userSaved._id,
-                firstName: userSaved.firstName,
-                lastName: userSaved.lastName,
-                email: userSaved.email,
-                createAt: userSaved.createdAt,
-                updateAt: userSaved.updatedAt
+    // Auth with google api
+    console.log("***************** REGISTER ****************")
+    console.log("access_token :",req.body.googleAccessToken)
+    if (req.body.googleAccessToken) {
+        axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+            headers: {
+                "Authorization": `Bearer ${req.body.googleAccessToken}`,
             }
+        }).then(async response => {
+            const { email, given_name, family_name, picture } = response.data;
+            const alreadyExistUser = await User.findOne({ email });
+            if (alreadyExistUser) {
+                return res.status(400).json({
+                    message: "User already exist"
+                })
+            }
+            const result = await User.create({ firstName: given_name, lastName: family_name, email: email, profilePicture: picture })
+            const token = jwt.sign({ email: result.email, id: result._id }, 'secreto', { expiresIn: '1h' });
+
+            res.status(200).json({
+                accessToken: token, user:result
+            })
+
+        }).catch(err => {
+            res.status(400).json({
+                message: "Invalid Info"
+            })
         })
 
+    } else
+    // Auth with JWT
+    {
+        const { firstName, lastName, email, password } = req.body
 
-    } catch (error) {
+        try {
+            const userFound = await User.findOne({ email })
+            if (userFound) return res.status(401).json({ message: 'This email has been taken' })
+            const hashedPassword = await bcryptjs.hash(password, 10);
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+                profilePicture:''
+            })
+            const userSaved = await newUser.save()
+            const token = jwt.sign({ email: userSaved.email, id:userSaved._id}, 'secreto', { expiresIn: '1h' });
 
-        return res.status(500).json({ message: error.message })
+            return res.json({
+                accessToken: token, user: userSaved
+            })
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
     }
 }
 
 
 async function login(req, res) {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (user && await bcryptjs.compare(password, user.password)) {
-        const token = jwt.sign({ email }, 'secreto', { expiresIn: '1h' });
-        return res.json({
-            accessToken: token, user: {
-                id: user._id,
-                email: user.email,
-                createAt: user.createdAt,
-                updateAt: user.updatedAt
+    console.log("***************** LOGIN ****************")
+    console.log("access_token :",req.body.googleAccessToken)
+    if (req.body.googleAccessToken) {
+        axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+            headers: {
+                "Authorization": `Bearer ${req.body.googleAccessToken}`
             }
+        }).then(async response => {
+            const { email } = response.data;
+            const alreadyExistUser = await User.findOne({ email });
+            if (!alreadyExistUser) {
+                return res.status(400).json({ message: "User don't exist!" })
+            }
+            const token = jwt.sign({ email: alreadyExistUser.email, id: alreadyExistUser._id }, 'secreto', { expiresIn: '1h' });
+            return res.json({
+                accessToken: token, user: alreadyExistUser
+            })
+
+        }).catch(err => {
+            res.status(400).json({
+                message: "Invalid Info to Login"
+            })
         })
+
     } else {
-        return res.status(401).json({ message: 'Credenciales inválidas' });
+        const { email, password } = req.body;
+
+        try {
+            const alreadyExistUser = await User.findOne({ email });
+
+            if (alreadyExistUser && await bcryptjs.compare(password, alreadyExistUser.password)) {
+                const token = jwt.sign({ email:alreadyExistUser.email, id:alreadyExistUser._id }, 'secreto', { expiresIn: '1h' });
+                return res.json({
+                    accessToken: token, user: alreadyExistUser
+                })
+            } else {
+                return res.status(401).json({ message: 'Credenciales inválidas' });
+            }
+        } catch (error) {
+
+        }
+
     }
 }
+
 
 export { register, login };
